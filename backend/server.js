6a518efdb4b8729c412d2d0f7e8d72140682821a
ABHIRAJ022 <rajput12557q@gmail.com>
2025-09-08 04:43:25 +0000

    database updated

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/paymentapp", {
+mongoose.connect("mongodb://localhost:27017/PaymentApp", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
